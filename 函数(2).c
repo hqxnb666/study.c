2e86ad92a2aaa3992fa7335037608ff859e516ee
hqxnb666 <130121049+hqxnb666@users.noreply.github.com>
2023-08-03 23:50:49 +0800

    Add files via upload

---
+++ 函数(2).c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//是闰年 返回1
+// 不是闰年 返回0
+//int is_leap_year(int y)
+//{
+//	if (((y % 4 == 0) && (y % 100 != 0)) || (y % 400 == 0))
+//		return 1;
+//	else
+//		return 0;
+//}
+int main()
+{
+	//函数的声明与定义
+	/*int y = 0;*/
+	//scanf("%d", &y);
+	//判断是否是闰年
+	/*if (is_leap_year(y))
+	{
+		printf("%d 是闰年\n", y);
+	}
+	else
+	{
+		printf("%d 不是闰年", y);
+	}*/
+
+
+
+	//static   extern  1可以用来 修饰局部变量，全局变量和函数
+
+	//在a.c文件中定义变量 add 想在另一个源文件中使用 需要extern
+	
+	
+
+	return 0;
+}
