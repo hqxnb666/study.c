ec450594b5a9573fedd90bc1f33a071ba4d13fe0
hqx <130121049+hqxnb666@users.noreply.github.com>
2024-07-29 20:35:45 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,136 +1,135 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-#include <string.h>
-#include <Windows.h>
 //int main()
 //{
-//	int num1[5] = { 1,2,3,4,5 };//完全初始化
-//	int num2[5] = { 1,2,3 };  //不完全初始化，剩余的元素初始化成0
-//	int num3[] = { 1,2,3 }; //如果数组初始化了，是可以省略掉数组大小的
-//	//数组的大小，是根据编译器初始化的内容来确定的
-//
-//	return 0;
-//}
-
-//int main()
-//{
-//	//   int num1[5] = { 1,2,3,4,5,6 };//err
-//	int num1[5] = { 1,2,3,4,5 }; //  num1的类型是   int [5]
-//	int num2[10] = { 1,2,3 };  //num2的类型是 int [5]
-//
-//
-//}
-
-//int main()
-//{
-//	int num1[10] = { 1,2,3,4,5,6,7,8,9,10 };
-//	//printf("%d\n", num1[6]);  //[]下标引用操作符
-//	//int n = 10;
-//	//int arr2[n];
-//
-//	//C99  标准之前数组的大小只能是常量指定，不能使用变量
-//	//C99  之后为什么就可以使用变量了  -- 变长数组
-//	/*for (int i = 0; i < 10; i++) {
-//		printf("%d ", num1[i]);
-//	}*/
-//
-//	for (int i = 0; i < 10; i++)
-//	{
-//		scanf("%d", &num1[i]);  //num1不需要&
-//	}
-//
-//	for (int i = 5; i < 10; i++)
-//	{
-//		printf("%d ", num1[i]);
-//	}
-//	return 0;
-//}
-
-
-//int main()
-//{
-//	int num1[10] = { 1,2,3,4,5,6,7,8,9,10 };
-//	for (int i = 0; i < 10; i++)
-//	{
-//		printf("&arr[%d] = %p\n", i, &num1[i]);
-//	}
-//	return 0;
-//}
-
-//int main()
-//{
-//	int arr[10] = { 1,2,3 }; //  char  1   short 2  int 4    4* 10 = 40
-//	printf("%zd\n", sizeof(arr)); //size(数组名)计算的是数组所占内存空间的大小，单位是字节
-//	printf("%zd\n", sizeof(arr[0])); 
-//	printf("%zd\n", sizeof(arr) / sizeof(arr[0])); //10
-//	size_t sz = sizeof(arr) / sizeof(arr[0]);
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	//            0 1 2 3 4 5 6 7 8 9
+//	int k = 0;
+//	scanf("%d", &k);
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int find = 0;
 //	for (int i = 0; i < sz; i++)
 //	{
-//		printf("%d ",arr[i]);
-//	}
-//	return 0;
-//}
-
-//int main()
-//{
-//	int arr1[][90] = { 1,2 };
-//
-//	int arr2[3][5] = { 0 };
-//	int arr3[3][5] = { 1,2,3,4,5,6,7 };
-//
-//	int arr4[3][5] = { {1,2},{3,4,5},{6,7} };
-//
-//	int arr[3][5] = { 1,2,3,4,5,
-//		              2,3,4,5,6,
-//		              3,4,5,6,7 };
-//	//printf("%d\n", arr[1][3]);  //下标引用操作符
-//	for (int i = 0; i < 3; i++)
-//	{
-//		for (int j = 0; j < 5; j++)
+//		if (k == arr[i])
 //		{
-//			//scanf("%d", &arr[i][j]);
-//			printf("&arr[%d][%d] = %p\n", i, j, &arr[i][j]);
+//			printf("找到了，下标是%d\n", i);
+//			find = 1;
+//			break;
 //		}
-//		//printf("\n");
+//	}
+//	if (find == 0)
+//	{
+//		printf("找不到\n");
 //	}
 //	return 0;
 //}
 
 //int main()
 //{
-//	int arr1[10];
-//	int arr2[3 + 5];
-//	//变长数组
-//	int n = 10;
-//	//int arr[n];
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int k = 0;
+//	scanf("%d", &k);
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	//查找--二分查找
+//	int find = 0;
+//	int left = 0;
+//	int right = sz - 1;
+//	while (left <= right)
+//	{
 //
-//	//VS虽然支持了C99的语法，但是不是全部支持的
-//	//比如变长数组在VS上就是不支持的  gcc编译器
+//		int mid = left + (right-left) / 2;
+//		
+//		//_CRT_INT_MAX;
+//		if (arr[mid] < k)
+//		{
+//			left = mid + 1;
+//		}
+//		else if (arr[mid] > k)
+//		{
+//			right = mid - 1;
+//		}
+//		else
+//		{
+//			printf("找到了，下标是:%d\n", mid);
+//			find = 1;
+//			break;
+//		}
+//	}
+//	if (find == 0)
+//	{
+//		printf("找不到了\n");
+//	}
+//	
 //	return 0;
 //}
 
+
+//int main()
+//{
+//	int sum = 0;
+//	int num[10] = {0};
+//	for (int i = 0; i < 10; i++)
+//	{
+//		scanf("%d", &num[i]);
+//		sum += num[i];
+//	}
+//	double average = sum / 10.0;
+//	printf("平均值为：%lf", average);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr1[10] = { 0 };
+//	int arr2[10] = { 0 };
+//	for (int i = 0; i < 10; i++)
+//	{
+//		scanf("%d", &arr1[i]);
+//		
+//
+//	}
+//	for (int i = 0; i < 10; i++)
+//	{
+//		scanf("%d", &arr2[i]);
+//
+//	}
+//	for(int i = 0; i<10; i++)
+//	{
+//		int temp = arr1[i];
+//		arr1[i] = arr2[i];
+//		arr2[i] = temp;
+//	}
+//
+//	return 0;
+//
+//}
+
+
+//int main()
+//{
+//	int a = 99;
+//	int b = 11;
+//	int c = 0;
+//
+//	while (c=a%b)
+//	{
+//		a = b;
+//		b = c;
+//	}
+//	printf("%d\n", b);
+//	return 0;
+//}
 
 int main()
 {
-	//  77788888888777
-	//  77777777777777
-	char arr1[] = "welcome to dongbei!!!!";
-	char arr2[] = "**********************";
-
-	int left = 0;
-	int right = strlen(arr1) - 1;
-
-	while (left <= right)
+	for (int year = 1000; year <= 2000;year++)
 	{
-		arr2[left] = arr1[left];
-		arr2[right] = arr1[right];
-		printf("%s\n", arr2);
-		Sleep(1000); //休眠 毫秒
-		system("cls");
-		left++;
-		right--;
+		if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+		{
+			printf("%d ", year);
+		}
 	}
+
 	
 	return 0;
-	
 }
