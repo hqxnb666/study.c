89facc77723d7badeb7dfade20b3501a493d2790
hqx <130121049+hqxnb666@users.noreply.github.com>
2024-08-08 23:51:18 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,8 +1,23 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include "my_list.h"
-#include <algorithm>
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+using namespace std;
+class Person {
+public:
+	virtual void BuyTIcket() { 
+		cout << "ÂòÆ±È«¼Û" << endl; }
+};
+
+class Student : public Person {
+public:
+	virtual void BuyTIcket() {
+		cout << "ÂòÆ±°ë¼Û" << endl;
+	}
+};
 int main()
 {
-	bit::test_list2();
+	Person p1;
+	Student s1;
+	p1.BuyTIcket();
+	s1.BuyTIcket();
 	return 0;
 }
