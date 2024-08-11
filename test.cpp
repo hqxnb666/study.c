a610235fea4074399019ae53dca6b1cd5cc494f0
hqx <130121049+hqxnb666@users.noreply.github.com>
2024-08-11 23:34:22 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 class Person {
 public:
-	virtual void BuyTIcket() { 
+	 void BuyTIcket() { 
 		cout << "买票全价" << endl; }
 };
 
@@ -13,6 +13,11 @@ public:
 		cout << "买票半价" << endl;
 	}
 };
+//多态的条件
+//虚函数的重写（父子类虚函数，要求三同  -- 函数名 参数 返回值）
+//                 1.例外（协变）
+//                 2.（析构函数）建议析构函数写成虚函数，防止内存泄漏
+  
 int main()
 {
 	Person p1;
