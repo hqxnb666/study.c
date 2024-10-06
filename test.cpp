84638b25a07f738829809fcb7982950cac26577d
hqx <130121049+hqxnb666@users.noreply.github.com>
2024-10-07 01:14:32 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,28 +1,17 @@
-#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-using namespace std;
-class Person {
-public:
-	 void BuyTIcket() { 
-		cout << "买票全价" << endl; }
-};
+#include <string>
+#include <algorithm>
 
-class Student : public Person {
-public:
-	virtual void BuyTIcket() {
-		cout << "买票半价" << endl;
-	}
-};
-//多态的条件
-//虚函数的重写（父子类虚函数，要求三同  -- 函数名 参数 返回值）
-//                 1.例外（协变）
-//                 2.（析构函数）建议析构函数写成虚函数，防止内存泄漏
-  
+using namespace std;
+
 int main()
 {
-	Person p1;
-	Student s1;
-	p1.BuyTIcket();
-	s1.BuyTIcket();
+	/*int n = 10;
+	cout << n << endl;
+	cin >> n;
+	string str;*/
+	int n = 2;
+	cin >> n;
 	return 0;
 }
