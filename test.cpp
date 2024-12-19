7359fd61284fbf569bc193e1e150246449e464de
hqx <130121049+hqxnb666@users.noreply.github.com>
2024-12-19 22:22:26 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,114 +1,17 @@
-#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <string>
+#include <algorithm>
+
 using namespace std;
-template<class K, class V>
-struct AVLTreeNode
-{
-	AVLTreeNode<K, V>* _left;
-	AVLTreeNode<K, V>* _right;
-	AVLTreeNode<K, V>* _parent;
-	pair<K, V> _kv;
-	int _bf; //balance factor
-	AVLTreeNode(const pair<K,V>& _kv)
-		:_left(nullptr)
-		,_right(nullptr)
-		,_parent(nullptr)
-		,_kv(_kv)
-		,_bf(0)
-	{}
-};
 
-template<class K, class V>
-class AVLTree
-{
-	typedef AVLTreeNode<K, V> Node;
-public:
-
-	bool insert(const pair<K, V>& kv)
-	{
-		if (_root == nullptr) {
-			_root = new Node(kv);
-			return true;
-		}
-		Node* parent = nullptr;
-		Node* cur = _root;
-		while (cur) {
-			if (cur->_kv.first < kv.first) {
-				parent = cur;
-				cur = cur->_right;
-			}
-			else if (cur->_kv.first > kv.first) {
-				parent = cur;
-				cur = cur->_left;
-			}
-			else
-			{
-				return false;
-			}
-		}
-
-		cur = new Node(kv);
-		if (parent->_kv.first < kv.first) {
-			parent->_right = cur;
-		}
-		else
-		{
-			parent->_left = cur;
-		}
-		cur->_parent = parent;//每次插入都和父亲链接上
-		//现在每次插入要更新平衡因子
-
-		while ()
-		{
-			if (cur == parent->_left) {
-				parent->_bf--;
-			}
-			else {
-				parent->_bf++;
-			}
-			if (parent->_bf == 0)
-			{
-				break;
-			}
-			else if (parent->_bf == 1 || parent->_bf == -1) {
-				//继续向上更新
-				cur = parent;
-				parent = parent->_parent;
-			}
-			else if (parent->_bf == 2 || parent->_bf == -2) {
-				//当前子树出问题了
-			}
-		}
-
-
-		return true;
-	}
-
-	Node* Find(const K& key)
-	{
-		Node* cur = _root;
-		while (cur)
-		{
-			if (cur->_kv.first < key)
-			{
-				cur = cur->_right;
-			}
-			else if (cur->_kv.first > key)
-			{
-				cur = cur->_left;
-			}
-			else
-			{
-				return cur;
-			}
-		}
-		return nullptr;
-	}
-
-private:
-	Node* _root;
-};
 int main()
 {
+	/*int n = 10;
+	cout << n << endl;
+	cin >> n;
+	string str;*/
+	int n = 2;
+	cin >> n;
 	return 0;
 }
