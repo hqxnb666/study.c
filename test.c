6204478e13225824087d60572d883149738ef540
YANG LI XIN <130121049+hqxnb666@users.noreply.github.com>
2025-01-16 16:18:33 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,19 +1,46 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-#include <stdio.h>
-void PrintMulTable(int N)
-{
-    for (int i = 1; i <= N; ++i)
-    {
-        for (int j = 1; j <= i; ++j)
-        {
-            printf("%d*%d=%2d  ", j, i, j * i);
-        }
-        printf("\n");
-    }
-}
+#include "SList.h"
+
 int main() {
-    int a;
-    scanf("%d", &a);
-    PrintMulTable(a);
+    SLNode* head = NULL;
+
+    // 测试尾插
+    SLTPushBack(&head, 1);
+    SLTPushBack(&head, 2);
+    SLTPushBack(&head, 3);
+    SLTPrint(head); // 输出: 1 -> 2 -> 3 -> NULL
+
+    // 测试头插
+    SLTPushFront(&head, 0);
+    SLTPrint(head); // 输出: 0 -> 1 -> 2 -> 3 -> NULL
+
+    // 测试尾删
+    SLTPopBack(&head);
+    SLTPrint(head); // 输出: 0 -> 1 -> 2 -> NULL
+
+    // 测试头删
+    SLTPopFront(&head);
+    SLTPrint(head); // 输出: 1 -> 2 -> NULL
+
+    // 测试查找
+    SLNode* found = SLTFind(head, 2);
+    if (found) {
+        printf("找到节点: %d\n", found->val); // 输出: 找到节点: 2
+    }
+    else {
+        printf("未找到节点\n");
+    }
+
+    // 测试插入
+    SLTInsert(&head, found, 4);
+    SLTPrint(head); // 输出: 1 -> 2 -> 4 -> NULL
+
+    // 测试删除
+    SLTErase(&head, found);
+    SLTPrint(head); // 输出: 1 -> 4 -> NULL
+
+    // 测试销毁
+    SLTDestroy(&head);
+    SLTPrint(head); // 输出: NULL
+
     return 0;
 }
